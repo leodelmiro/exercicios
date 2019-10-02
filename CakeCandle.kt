14a34930e9e5274dba5aff582126609125c56c58
Leonardo Delmiro <leodelmiroms@hotmail.com>
2019-10-01 21:28:22 -0300

    CakeCandle Kotlin File

---
+++ CakeCandle.kt
@@ -0,0 +1,26 @@
+package hackerRank
+
+
+import java.util.*
+import kotlin.collections.*
+import kotlin.io.*
+import kotlin.text.*
+
+// Complete the birthdayCakeCandles function below.
+fun birthdayCakeCandles(ar: Array<Int>): Int {
+    val tallestCandle = ar.max()
+    val tallestCandleQuantity = ar.count{it == tallestCandle}
+    return tallestCandleQuantity
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val arCount = scan.nextLine().trim().toInt()
+
+    val ar = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+
+    val result = birthdayCakeCandles(ar)
+
+    println(result)
+}

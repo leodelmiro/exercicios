238e09c2e9e3af7382858fc5547dbd68bb88b740
Leonardo Delmiro <leodelmiroms@hotmail.com>
2019-10-05 22:49:47 -0300

    Adicionando Staircase.kt

---
+++ Staircase.kt
@@ -0,0 +1,22 @@
+package hackerRank
+
+import java.util.*
+import kotlin.io.*
+import kotlin.text.*
+
+// Complete the staircase function below.
+fun staircase(n: Int): Unit {
+    val symbol = "#"
+    val space = " "
+    for (i in 1..n){
+        println(space.repeat(n-i).plus(symbol.repeat(i)))
+    }
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val n = scan.nextLine().trim().toInt()
+
+    staircase(n)
+}
